e0c4755b510acfdafb728a2f098d8d28f01cd3d7
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-03-30 21:35:53 +0800

    5accept

---
+++ 5LongestPalindromicSubstring/src/org/intel/dcg/Main.java
@@ -0,0 +1,7 @@
+package org.intel.dcg;
+public class Main{
+	public static void main(String[] args){
+		Solution mySolution = new Solution();
+		System.out.println(mySolution.longestPalindrome("cbbd"));
+	}
+}
---
+++ 5LongestPalindromicSubstring/src/org/intel/dcg/Solution.java
@@ -0,0 +1,33 @@
+package org.intel.dcg;
+class Solution {
+    public String longestPalindrome(String s) {
+    	if(s == "" || s.length() == 1){
+    		return s;
+    	}
+    	String subString = s;
+    	int totalLength = s.length();
+    	int currentlength = s.length();
+        while(currentlength > 0){
+        	for(int i =0;i<totalLength-currentlength+1;i++){
+        		if(isPalindrome(s.substring(i,i+currentlength))){
+        			return s.substring(i,i+currentlength);
+        		}
+        	}
+        	currentlength -= 1;
+        }
+        return "";
+    }
+    boolean isPalindrome(String s){
+    	int start = 0;
+    	int end = s.length()-1;
+    	while(start<end){
+    		if(s.charAt(start) != s.charAt(end)){
+    			return false;
+    		}else{
+    			start +=1;
+    			end -=1;
+    		}
+    	}
+    	return true;
+    }
+}
