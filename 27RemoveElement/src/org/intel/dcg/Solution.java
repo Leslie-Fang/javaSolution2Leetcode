95345a83025f7140a6bb5815fd9363a0f1b92c01
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-04-05 09:41:01 +0800

    27accept

---
+++ 27RemoveElement/src/org/intel/dcg/Solution.java
@@ -0,0 +1,14 @@
+ package org.intel.dcg;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    	int point = 0;
+    	for(int i:nums){
+    		if(i!=val){
+    			nums[point] = i;
+    			point +=1;
+    		}
+    	}
+    	return point;
+        
+    }
+}
