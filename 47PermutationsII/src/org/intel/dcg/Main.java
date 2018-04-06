f5d351d7cf03e0f746fae7bdc636a89cbb6c67ca
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-04-06 09:21:00 +0800

    47accept

---
+++ 47PermutationsII/src/org/intel/dcg/Main.java
@@ -0,0 +1,9 @@
+package org.intel.dcg;
+class Main{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		Solution mySolution = new Solution();
+		int[] input = new int[]{1,1,2,2};
+		System.out.println(mySolution.permuteUnique(input));
+	}
+}
